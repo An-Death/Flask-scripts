cc6dad81c9686fa3ea59b44fb7215575ec5e5c26
An-Death <saenarayoursorrow@gmail.com>
2017-12-01 02:19:44 +0300

    User_report:  Автоматически определяем дефолтные даты для таблицы (-12 недель месяца от текущейдаты) и берём 01 число в обоих случаях

--- scripts/users_activity_report/forms.py
+++ scripts/users_activity_report/forms.py
@@ -1,8 +1,13 @@
+from datetime import datetime, timedelta
+
 from flask_wtf import FlaskForm
 from wtforms import DateField
 from wtforms.validators import DataRequired
 
+# По умолчанию берётся начало текущего месца и 01 число трем месяцами ранее (квартал)
+default_dt_from = lambda: (datetime.today() - timedelta(weeks=12)).replace(day=1)
+default_dt_to = lambda: datetime.today().replace(day=1)
 
 class ChoseDateForm(FlaskForm):
-    date_from = DateField('From', validators=[DataRequired()])
-    date_to = DateField('To', validators=[DataRequired()])
+    date_from = DateField('From', validators=[DataRequired()], default=default_dt_from)
+    date_to = DateField('To', validators=[DataRequired()], default=default_dt_to)
