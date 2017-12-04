70472b5597db3c66e206da8346a79cc818561783
An-Death <saenarayoursorrow@gmail.com>
2017-12-04 09:06:08 +0300

    Добавил комментариев, вернул shortcut, well_name для последующего запроса скрипта

--- scripts/param_table/views.py
+++ scripts/param_table/views.py
@@ -1,72 +1,57 @@
-import datetime
 from pathlib import Path
 
-from flask import Blueprint, request, render_template, send_file, url_for, redirect, flash, g
+from flask import Blueprint, request, render_template, send_file, url_for, redirect, flash
 from sqlalchemy.exc import OperationalError
 
 from models.models import *
-from .forms import ParamTableForm
 from scripts.param_table import scr
+from scripts.param_table.scr import DateLimit
 
 pt = Blueprint('param_table', __name__, url_prefix='/scripts/param_table')
-
-class DateLimit:
-    def __init__(self, val=2, limit='days'):
-        if limit == 'weeks':
-            self._val = val*7
-        else:
-            self._val = val
-        self._date_to = datetime.datetime.now()
-        self._date_from = self._date_to - datetime.timedelta(days=self._val)
-
-    @property
-    def end(self):
-        return self._date_to
-    @property
-    def begin(self):
-        return self._date_from
 
 
 @pt.route('/<regex("[0-9]+|''"):network_id>', methods=['GET', 'POST'])
 def param_table(network_id=None):
     __title__ = 'Param Table'
     projects = Project.query.filter(Project.supported == 1).all()
-
-    if 'well_id' not in request.values:
+    # GET:
+    # if 'well_id' not in request.values:
+    if request.method == 'GET':
         if not network_id:
+            # Return list of {projects}
             return render_template('param_table/param_table.html', vars=locals())
         else:
-            project = Project.query.filter(Project.network_id == network_id).one()
+            # Get project from list by network_id
+            project = [project for project in projects if project.network_id == network_id][0]
+            # project = Project.query.filter(Project.network_id == network_id).one()
             try:
+                # Getting active wells from current project
                 wells = project.get_active_wells()
             except OperationalError:
                 flash(f'База данных проекта {project.name_ru} недоступна!')
                 return redirect(url_for('.param_table', network_id=''))
-            return render_template('param_table/param_table.html', vars=locals())
-    else:
-        # prj_name = request.values.get('project_name')
-        # prj = [prj for prj in projects if prj.name_ru == prj_name]
-        # server = prj[0].server
-        # shortcuts = server.shortcuts
-        # short = shortcuts.split(',')[0]
-        # network_id = form.project.data
-        # well_name = form.well.data
-        # list_of_records = form.records.data
-        # limit_selector = form.limit_selector.data
-        # # todo Написать условия для limit_selector
-        project = Project.query.filter(Project.network_id == network_id).one()
+            finally:
+                return render_template('param_table/param_table.html', vars=locals())
+    else:  # POST:
+        project = [project for project in projects if project.network_id == network_id][0]
+        # Get data from POST request
         well_id = request.values.get('well_id')
-        list_of_records = request.values.get('records', [1, 11, 12])
+        list_of_records = scr.return_list_from_str(request.values.get('records', [1, 11, 12]))
         limit = request.values.get('limit', 'weeks')
         limit_value = request.values.get('limit_value', 2)
+        # Prepare date limits
         dt = DateLimit(limit_value, limit)
+        # if no limits in POST request set default limits
         limit_from = request.values.get('limit_from', dt.begin)
         limit_to = request.values.get('limit_to', dt.end)
-        well = project.get_well_by_id(well_id)
+
         record_tables = {}
+        well = project.get_well_by_id(well_id)
+        # Are well does exist?
         if not well :
             flash(f'Скважины с id:{well_id} на проекте {project.name_ru} нет!')
             return redirect(url_for('.param_table', network_id=network_id))
+        # Well exist... well keep checking
         for record in list_of_records[:]:
             tables = well.check_record_tables(record)
             if tables is None:
@@ -74,29 +59,36 @@ def param_table(network_id=None):
                       f'- Не существует. Удаляем рекорд: {record} из списка: {list_of_records}')
                 list_of_records.remove(record)
             else:
+                # Saving sqla.Tables in dict
                 record_tables[record] = tables
+        # List is empty?
         if not list_of_records:
-            flash(f'Отсутствуют данные в скважние {well.name} '
-                  f'по заданным рекордам {request.values.get("records", [1, 11, 12])} ')
+            flash(f'Отсутствуют данные в скважние {well.name}\n'
+                  f'Проверьте скважину или список рекордов: {request.values.get("records", [1, 11, 12])}')
             return redirect(url_for('.param_table', network_id=network_id))
-        for r, tables in record_tables.items():
-            idx = tables['idx']
-            data = tables['data']
-            q = idx.query.join(data, idx.id == data.idx_id).filer(idx.date > limit_from).filter(idx.date < limit_to)
-            if q.count() < 0:
-                flash(f'Отсутствуют данные в таблицах: {idx} || {data}... '
-                      f'Удаляем рекорд: {r} из списка: {list_of_records}')
-                list_of_records.pop(r)
+        # List is not empty...
+        # todo sa.Table haven't query method!!!
+        # for r, tables in record_tables.items():
+        #     idx = tables['idx']
+        #     data = tables['data']
+        #     # todo sqla.Table have not method query. how i can add it?
+        #     q = idx.query.join(data, idx.id == data.idx_id).filer(idx.date > limit_from).filter(idx.date < limit_to)
+        #     if q.count() < 0:
+        #         flash(f'Отсутствуют данные в таблицах: {idx} || {data}... '
+        #               f'Удаляем рекорд: {r} из списка: {list_of_records}')
+        #         list_of_records.remove(r)
         if not list_of_records:
-            flash(f'Отсутствуют данные в скважние {well.name} '
-                  f'по заданным рекордам {request.values.get("records", [1, 11, 12])} ')
+            flash(f'Отсутствуют данные в скважние {well.name} \n'
+                  f'Проверьте скважину или список рекордов: {request.values.get("records", [1, 11, 12])}')
             return redirect(url_for('.param_table', network_id=network_id))
-        # list_records = request.values.get('records', '1,11,12').split(',')
-        return redirect(url_for('param_table.download_param_table', network_id=network_id, well_id=well_id))
+        well_name = well.name or well.alias
+        shortcut = project.shortcut
+        return redirect(url_for('param_table.download_param_table', shortcut=shortcut, well_name=well_name))
+        # return redirect(url_for('param_table.download_param_table', network_id=network_id, well_id=well_name))
 
 
-@pt.route('/download_param_table/<network_id>/<well_id>', methods=['GET', 'POST'])
-def download_param_table(network_id=None, well_id=None):
+@pt.route('/download_param_table/<shortcut>/<well_name>', methods=['GET', 'POST'])
+def download_param_table(shortcut=None, well_name=None):
     __title__ = 'Download param table'
     if request.method == 'GET':
         key = True
